9c8235fa87b68bedc6c28f4e38ce0e06394f3a6a
Pavel Zhur <completely.real@gmail.com>
2024-06-20 03:23:30 +0300

    fixing the powershell simd compatiblity publishing script

--- OneShelf.Frontend/OneShelf.Frontend.Web/publish.ps1
+++ OneShelf.Frontend/OneShelf.Frontend.Web/publish.ps1
@@ -9,8 +9,8 @@ dotnet publish --nologo --no-restore --configuration Release --output "bin\Publi
 
 # Combine builds
 # Copy the 'wwwroot\_framework' folder contents from the 2nd build to 'wwwroot\_frameworkCompat' in the 1st build
-xcopy /I /E /Y "bin\PublishCompat\wwwroot\_framework" "bin\Publish\wwwroot\_frameworkCompat"
+Copy-Item -Path "bin\PublishCompat\wwwroot\_framework" -Destination "bin\Publish\wwwroot\_frameworkCompat" -Recurse
 
 # If building a PWA app with server-worker-assets.js the service-worker script needs to be modified to also detect SIMD and cache the appropriate build
 # Copy the service-worker-assets.js from the 2nd build to 'service-worker-assets-compat.js' of the 1st build
-copy /Y "bin\PublishCompat\wwwroot\service-worker-assets.js" "bin\Publish\wwwroot\service-worker-assets-compat.js"
+Copy-Item -Path "bin\PublishCompat\wwwroot\service-worker-assets.js" -Destination "bin\Publish\wwwroot\service-worker-assets-compat.js"
