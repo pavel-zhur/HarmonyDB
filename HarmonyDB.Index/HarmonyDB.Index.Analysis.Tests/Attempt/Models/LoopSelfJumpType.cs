33dee0391f9f5ecd50b679d0dc2770a722e88e9d
Pavel Zhur <completely.real@gmail.com>
2024-07-02 10:13:00 +0000

    added and proved 5 distict statistically significant self jump types

---
+++ HarmonyDB.Index/HarmonyDB.Index.Analysis.Tests/Attempt/Models/LoopSelfJumpType.cs
@@ -0,0 +1,10 @@
+﻿namespace HarmonyDB.Index.Analysis.Tests.Attempt.Models;
+
+public enum LoopSelfJumpType
+{
+    ModulationJointLoop,
+    ModulationJointMovement,
+    ModulationAmbiguousChord,
+    ModulationOverlap,
+    SameKeyJointLoop,
+}
