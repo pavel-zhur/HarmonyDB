767d25621c9bb86356e068bf9ccd9f1ba2f9ff59
Pavel Zhur <completely.real@gmail.com>
2024-07-04 13:23:53 +0000

    best tonalities in cache (index headers) support

--- HarmonyDB.Source/HarmonyDB.Source.Api.Model/V1/IndexHeader.cs
+++ HarmonyDB.Source/HarmonyDB.Source.Api.Model/V1/IndexHeader.cs
@@ -1,14 +1,6 @@
 namespace HarmonyDB.Source.Api.Model.V1;
 
-public record IndexHeader
+public record IndexHeader : IndexHeaderBase
 {
-    public required string ExternalId { get; set; }
-
-    public required string Source { get; set; }
-
-    public required float? Rating { get; init; }
-
-    public required string? Title { get; init; }
-
-    public required IReadOnlyList<string>? Artists { get; init; }
+    public required IndexHeaderBestTonality? BestTonality { get; init; }
 }
---
+++ HarmonyDB.Source/HarmonyDB.Source.Api.Model/V1/IndexHeaderBase.cs
@@ -0,0 +1,14 @@
+namespace HarmonyDB.Source.Api.Model.V1;
+
+public record IndexHeaderBase
+{
+    public required string ExternalId { get; set; }
+
+    public required string Source { get; set; }
+
+    public required float? Rating { get; init; }
+
+    public required string? Title { get; init; }
+
+    public required IReadOnlyList<string>? Artists { get; init; }
+}
