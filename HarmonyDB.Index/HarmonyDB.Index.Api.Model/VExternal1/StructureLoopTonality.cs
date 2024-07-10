6e7de58c5120c55313710cc030c24edd5d05d0fc
Pavel Zhur <completely.real@gmail.com>
2024-07-10 16:18:45 +0300

    cosmetic

--- HarmonyDB.Index/HarmonyDB.Index.Api.Model/VExternal1/StructureLoopTonality.cs
+++ HarmonyDB.Index/HarmonyDB.Index.Api.Model/VExternal1/StructureLoopTonality.cs
@@ -3,15 +3,21 @@ using HarmonyDB.Index.Analysis.Models.Index;
 namespace HarmonyDB.Index.Api.Model.VExternal1;
 
 public record StructureLoopTonality(
+
+    // base
     string Normalized,
     int Length,
     int TotalOccurrences,
     int TotalSuccessions,
     int TotalSongs,
+
+    // new
     float[] Probabilities,
     float ScaleScore,
     float TonicScore)
-    : StructureLoop(Normalized,
+
+    : StructureLoop(
+        Normalized,
         Length, 
         TotalOccurrences,
         TotalSuccessions,
--- HarmonyDB.Index/HarmonyDB.Index.Api.Model/VExternal1/StructureSongTonality.cs
+++ HarmonyDB.Index/HarmonyDB.Index.Api.Model/VExternal1/StructureSongTonality.cs
@@ -4,10 +4,15 @@ using HarmonyDB.Source.Api.Model.V1;
 namespace HarmonyDB.Index.Api.Model.VExternal1;
 
 public record StructureSongTonality(
+
+    // base
     string ExternalId,
     int TotalLoops,
+
+    // new
     float[] Probabilities,
     float ScaleScore,
     float TonicScore,
     IndexHeader IndexHeader)
+
     : StructureSong(ExternalId, TotalLoops);
