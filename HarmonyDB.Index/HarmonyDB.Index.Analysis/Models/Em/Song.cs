bb620da25da59facb782d1a15e060a424df14f7e
Pavel Zhur <completely.real@gmail.com>
2024-07-13 04:08:51 +0300

    cosmetic

--- HarmonyDB.Index/HarmonyDB.Index.Analysis/Models/Em/Song.cs
+++ HarmonyDB.Index/HarmonyDB.Index.Analysis/Models/Em/Song.cs
@@ -4,8 +4,8 @@ namespace HarmonyDB.Index.Analysis.Models.Em;
 
 public class Song : ISource, ISong
 {
-    public required string Id { get; set; }
+    public required string Id { get; init; }
     public float[,] TonalityProbabilities { get; set; } = new float[Constants.TonicCount, Constants.ScaleCount];
     public (float TonicScore, float ScaleScore) Score { get; set; }
-    public required (byte Tonic, Scale Scale)? KnownTonality { get; set; }
+    public required (byte Tonic, Scale Scale)? KnownTonality { get; init; }
 }
