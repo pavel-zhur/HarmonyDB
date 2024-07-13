6d7c95861018d4b0818ac1e93c50e29836c4b7a0
Pavel Zhur <completely.real@gmail.com>
2024-07-13 00:28:36 +0000

    average coverage percentiles

--- HarmonyDB.Index/HarmonyDB.Index.Api.Model/VExternal1/StructureLoopsResponseDistributions.cs
+++ HarmonyDB.Index/HarmonyDB.Index.Api.Model/VExternal1/StructureLoopsResponseDistributions.cs
@@ -5,6 +5,7 @@ public record StructureLoopsResponseDistributions
     public required int[] TotalSongs { get; init; }
     public required float[] TotalOccurrences { get; init; }
     public required float[] TotalSuccessions { get; init; }
+    public required float[] AverageCoverage { get; init; }
     public required float[] TonalityConfidence { get; init; }
     public required float[] TonicConfidence { get; init; }
     public required float[] TonicScore { get; init; }
