b8e691e097e39db55de172885477bd1627253140
Pavel Zhur <completely.real@gmail.com>
2024-07-15 00:43:07 +0000

    made the master code conditional (for the local development)

--- OneShelf.Common/OneShelf.Common.Api.Client/ApiClientOptions.cs
+++ OneShelf.Common/OneShelf.Common.Api.Client/ApiClientOptions.cs
@@ -10,11 +10,10 @@ public class ApiClientOptions<TClient> : ApiClientOptionsEndpoint<TClient>
                : ConditionalStreams?.GetValueOrDefault(conditionalStreamId)?.Endpoint ?? Endpoint)
            ?? throw new($"The endpoint for conditional stream '{conditionalStreamId}' is not configured, the default fallback value is not available, either.");
 
-    public string GetMasterCode(string? conditionalStreamId)
-        => (conditionalStreamId == null
-               ? MasterCode
-               : ConditionalStreams?.GetValueOrDefault(conditionalStreamId)?.MasterCode ?? MasterCode)
-           ?? throw new($"The master code for conditional stream '{conditionalStreamId}' is not configured, the default fallback value is not available, either.");
+    public string? GetMasterCode(string? conditionalStreamId)
+        => conditionalStreamId == null
+            ? MasterCode
+            : ConditionalStreams?.GetValueOrDefault(conditionalStreamId)?.MasterCode ?? MasterCode;
 
     public string GetServiceCode(string? conditionalStreamId)
         => (conditionalStreamId == null
