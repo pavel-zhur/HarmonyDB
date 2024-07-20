faefc20e89fd23961a5ab1b8b6852f4f86ef5804
Pavel Zhur <completely.real@gmail.com>
2024-07-20 06:23:48 +0300

    todo revert or review: multi page model

---
+++ HarmonyDB.Playground/HarmonyDB.Playground.Web/Models/Structures/MultiModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HarmonyDB.Playground.Web.Models.Structures;
+
+public record MultiModel
+{
+    public List<string> ExternalIds { get; set; } = new();
+    
+    public int? ToRemove { get; set; }
+
+    public bool IncludeTrace { get; init; }
+}
