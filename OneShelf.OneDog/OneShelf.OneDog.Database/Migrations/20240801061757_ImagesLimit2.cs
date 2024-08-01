9b5184964c623ca1a06e572565a3e1c0b292308d
Pavel Zhur <completely.real@gmail.com>
2024-08-01 09:18:55 +0300

    images limit bigint

---
+++ OneShelf.OneDog/OneShelf.OneDog.Database/Migrations/20240801061757_ImagesLimit2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneShelf.OneDog.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImagesLimit2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ImagesLimit_Window",
+                table: "Domains",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "ImagesLimit_Window",
+                table: "Domains",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
--- OneShelf.OneDog/OneShelf.OneDog.Database/Model/ImagesLimit.cs
+++ OneShelf.OneDog/OneShelf.OneDog.Database/Model/ImagesLimit.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OneShelf.OneDog.Database.Model;
 
 [Owned]
 public class ImagesLimit
 {
+    [Column(TypeName = "bigint")]
     public required TimeSpan Window { get; init; }
     
     public required int Limit { get; init; }
