22cc95781a5d6aada1b50750f7a4b62fae9fbf94
Pavel Zhur <completely.real@gmail.com>
2024-08-18 01:29:23 +0300

    ca

--- OneShelf.OneDog/OneShelf.OneDog.Processor/ServiceCollectionExtensions.cs
+++ OneShelf.OneDog/OneShelf.OneDog.Processor/ServiceCollectionExtensions.cs
@@ -40,8 +40,6 @@ public static class ServiceCollectionExtensions
                 );
 
         services
-            .AddScoped<ChannelActions>()
-
             .AddOpenAi(configuration);
 
         return services;
