dcd0a94e26525feae3fe9340aa06d7135838d15e
Pavel Zhur <completely.real@gmail.com>
2024-08-18 17:45:51 +0300

    db management functions

--- OneShelf.OneDragon/OneShelf.OneDragon.Runner.Functions/Functions/ManagementFunctions.cs
+++ OneShelf.OneDragon/OneShelf.OneDragon.Runner.Functions/Functions/ManagementFunctions.cs
@@ -2,8 +2,10 @@ using System.Net;
 using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using OneShelf.OneDragon.Database;
 using OneShelf.OneDragon.Processor.Model;
 using Telegram.BotAPI;
 using Telegram.BotAPI.GettingUpdates;
@@ -15,10 +17,12 @@ namespace OneShelf.OneDragon.Runner.Functions.Functions
         private readonly ILogger<ManagementFunctions> _logger;
         private readonly TelegramOptions _telegramOptions;
         private readonly TelegramBotClient _api;
+        private readonly DragonDatabase _dragonDatabase;
 
-        public ManagementFunctions(ILogger<ManagementFunctions> logger, IOptions<TelegramOptions> telegramOptions)
+        public ManagementFunctions(ILogger<ManagementFunctions> logger, IOptions<TelegramOptions> telegramOptions, DragonDatabase dragonDatabase)
         {
             _logger = logger;
+            _dragonDatabase = dragonDatabase;
             _telegramOptions = telegramOptions.Value;
             _api = new(_telegramOptions.Token);
         }
@@ -32,6 +36,34 @@ namespace OneShelf.OneDragon.Runner.Functions.Functions
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
             await response.WriteStringAsync("Welcome to Azure Functions!");
+
+            return response;
+        }
+
+        [Function(nameof(CheckDb))]
+        public async Task<HttpResponseData> CheckDb([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            await response.WriteStringAsync($"Welcome to Azure Functions! Users Count = {await _dragonDatabase.Users.CountAsync()}");
+
+            return response;
+        }
+
+        [Function(nameof(MigrateDb))]
+        public async Task<HttpResponseData> MigrateDb([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            await _dragonDatabase.Database.MigrateAsync();
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            await response.WriteStringAsync("Done.");
 
             return response;
         }
