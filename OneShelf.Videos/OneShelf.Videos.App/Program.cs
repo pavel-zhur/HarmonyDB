5cc72d0f0a2d8056fdbadd72ad67993760e89d20
Pavel Zhur <completely.real@gmail.com>
2024-08-21 05:36:08 +0300

    google photos library added

--- OneShelf.Videos/OneShelf.Videos.App/Program.cs
+++ OneShelf.Videos/OneShelf.Videos.App/Program.cs
@@ -1,62 +1,17 @@
-﻿using System.Globalization;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using CsvHelper;
-using OneShelf.Videos.App.ChatModels;
-using OneShelf.Videos.App.Models;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OneShelf.Videos.App;
 
-namespace OneShelf.Videos.App;
+var builder = Host.CreateApplicationBuilder();
+builder.Configuration.AddJsonFile("appsettings.Secrets.json");
+builder.Services
+    .AddScoped<Service1>()
+    .AddScoped<Service2>()
+    .AddGooglePhotos();
+var host = builder.Build();
 
-public class Program
-{
-    static async Task Main()
-    {
-        var path = "C:\\temp\\telegram google photos";
-        var files = Directory.GetFiles(path, "result*.json");
-        var chats = files.Select(f =>
-        {
-            try
-            {
-                return JsonSerializer.Deserialize<Chat>(File.ReadAllText(f), new JsonSerializerOptions
-                {
-                    UnmappedMemberHandling = JsonUnmappedMemberHandling.Disallow,
-                    PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
-                });
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(f);
-                throw;
-            }
-        }).ToList();
+var service1 = host.Services.GetRequiredService<Service1>();
+var service2 = host.Services.GetRequiredService<Service2>();
 
-        //await ListActualFiles(path);
-        var actualFiles = ReadActualFiles(path);
-
-        //await ExportMessages(path, chats);
-    }
-
-    private static async Task ExportMessages(string path, List<Chat?> chats)
-    {
-        using var fileStream = File.Create(Path.Combine(path, "messages.csv"));
-        using var textWriter = new StreamWriter(fileStream);
-        using var csvWriter = new CsvWriter(textWriter, CultureInfo.InvariantCulture);
-        await csvWriter.WriteRecordsAsync(chats.SelectMany(c => c.Messages));
-    }
-
-    private static List<ActualFile> ReadActualFiles(string path)
-    {
-        return JsonSerializer.Deserialize<List<ActualFile>>(File.ReadAllText(GetAllJsonPath(path)))!;
-    }
-
-    private static async Task ListActualFiles(string path)
-    {
-        var actualFiles = new DirectoryInfo("y:").GetFiles("*.*", SearchOption.AllDirectories).Select(x => new ActualFile(x.FullName, x.Length)).ToList();
-        await File.WriteAllTextAsync(GetAllJsonPath(path), JsonSerializer.Serialize(actualFiles, new JsonSerializerOptions { WriteIndented = true, }));
-    }
-
-    private static string GetAllJsonPath(string path)
-    {
-        return Path.Combine(path, "all.json");
-    }
-}
+await service2.LoginAndListAlbums();
---
+++ OneShelf.Videos/OneShelf.Videos.App/Service2.cs
@@ -0,0 +1,28 @@
+﻿using CasCap.Services;
+using Microsoft.Extensions.Logging;
+
+namespace OneShelf.Videos.App;
+
+public class Service2
+{
+    private readonly GooglePhotosService _googlePhotosService;
+    private readonly ILogger<Service2> _logger;
+
+    public Service2(GooglePhotosService googlePhotosService, ILogger<Service2> logger)
+    {
+        _googlePhotosService = googlePhotosService;
+        _logger = logger;
+    }
+
+    public async Task LoginAndListAlbums()
+    {
+        if (!await _googlePhotosService.LoginAsync())
+            throw new("login failed");
+
+        var albums = await _googlePhotosService.GetAlbumsAsync();
+        foreach (var album in albums)
+        {
+            _logger.LogInformation($"{album.id}\t{album.title}");
+        }
+    }
+}
