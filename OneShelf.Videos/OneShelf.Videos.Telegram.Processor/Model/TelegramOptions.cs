43515f61819609e4f19191d3e04b8d6899e890cd
Pavel Zhur <completely.real@gmail.com>
2024-09-08 10:25:01 +0300

    removed the bot

--- OneShelf.Videos/OneShelf.Videos.Telegram.Processor/Model/TelegramOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OneShelf.Videos.Telegram.Processor.Model;
-
-public record TelegramOptions : OneShelf.Telegram.Options.TelegramOptions
-{
-    public required string Token { get; init; }
-
-    public required string WebHooksSecretToken { get; init; }
-}
--- OneShelf.Videos/OneShelf.Videos.Telegram.Processor/Services/VideosCollectorMemory.cs
+++
@@ -1,8 +0,0 @@
-﻿using Nito.AsyncEx;
-
-namespace OneShelf.Videos.Telegram.Processor.Services;
-
-public class VideosCollectorMemory
-{
-    public AsyncLock DatabaseLock { get; } = new();
-}
