78f80f6bb64e4ebab3e4d549870a736b2627fbad
pavel <completely.real@gmail.com>
2025-05-11 02:59:10 +0300

    AllowAnyServerCertificate

--- OneShelf.Common/OneShelf.Common.Cosmos/Options/CosmosDatabaseOptions.cs
+++ OneShelf.Common/OneShelf.Common.Cosmos/Options/CosmosDatabaseOptions.cs
@@ -7,4 +7,6 @@ public class CosmosDatabaseOptions
     public required string PrimaryKey { get; init; }
 
     public required string DatabaseName { get; init; }
+
+    public bool AllowAnyServerCertificate { get; init; } = false;
 }
